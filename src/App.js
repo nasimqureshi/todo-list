c87530c95b101cffea38513185a4f35b31162d38
nasimqureshi <qureshi.nasim@yahoo.com>
2023-04-15 00:23:28 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState([])
+  const [newTask, setNewTask] = useState("")
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value)
+  }
+  const addTask = () => {
+   setTodoList([...todoList, newTask])
+ }
+ const deleteTask = (taskName) => {
+   setTodoList(todoList.filter((task) => task !== taskName));
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='addTask'>
+      <input onChange={handleChange} />
+      <br></br>
+      <button onClick={addTask}> Add Task </button>
+     </div>
+     <div className='list'></div>
+      {todoList.map((task) =>{
+        return (
+          <div>
+          <h1>{task}</h1>
+          <button onClick={() => deleteTask(task)}>  X </button>
+          </div>
+        )
+      })}
     </div>
   );
 }
